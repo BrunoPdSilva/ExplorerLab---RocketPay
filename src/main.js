aee44d32fc25c0b434010bff4f0bde210dbbbb1a
Bruno Peres <bruno_16515@hotmail.com>
2022-10-17 21:16:39 -0300

    :boom: Explorer Lab - Aula 01 finalizada

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-import "./css/index.css"
+import "./css/index.css";
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img");
+
+const colors = {
+  visa: ["#436D99", "#2D57F2"],
+  mastercard: ["#DF6F29", "#C69347"],
+  default: ["black", "gray"],
+};
+
+function setCardType(cardType) {
+  ccBgColor01.setAttribute("fill", colors[cardType][0]);
+  ccBgColor02.setAttribute("fill", colors[cardType][1]);
+  ccLogo.setAttribute("src", `cc-${cardType}.svg`);
+}
+
+setCardType("visa");
+
+globalThis.setCardType = setCardType;
